fd0835dd6d00fdb8bca2b597cafe221ff9f96a79
Worawiboon <57031266+Worawiboon@users.noreply.github.com>
2020-02-03 11:53:03 +0700

    Update 1

---
+++ app/src/main/java/th/ac/su/ict/multiactivity/SecondActivity.kt
@@ -0,0 +1,38 @@
+package th.ac.su.ict.multiactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val name = intent.getStringExtra("name")
+        val gpa = intent.getDoubleExtra("gpa",-1.0)
+        val id = intent.getIntExtra("id",-1)
+
+        var tvDetail = findViewById<TextView>(R.id.tvDetail);
+
+        tvDetail.setText(name+"\n"+id+"\n"+gpa)
+
+        var btnShare = findViewById<Button>(R.id.btnShare)
+        btnShare.setOnClickListener{
+
+            val value = name+"\n"+id+"\n"+gpa
+
+            var intent = Intent();
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT,value)
+            intent.type = "text/plan"
+
+            startActivity(Intent.createChooser(intent,"share info"))
+
+        }
+
+    }
+}
